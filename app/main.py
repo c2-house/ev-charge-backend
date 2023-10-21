747b39146e742460598b703219fd399add8b82f3
Jungminchae <minchae3618@gmail.com>
2023-10-22 00:39:01 +0900

    Add Cors Middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.routers import router as api_router
 
 
 app = FastAPI()
 
 app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://ev-charge.kr", "http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
